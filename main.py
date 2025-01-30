099fc339ceb090197a3d8193868d4e340f42bb16
Adarsh Latiyan <adarshlatiyan@aesl.in>
2025-01-30 22:00:32 +0530

    Triangle player drawing and rotating

--- main.py
+++ main.py
@@ -3,19 +3,33 @@
 # throughout this file
 import pygame
 import constants
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    dt = 0
+    clock = pygame.time.Clock()
     
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+            
 
-        
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
-        main()
+    main()
